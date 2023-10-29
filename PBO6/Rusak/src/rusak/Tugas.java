bfaa45fac7583b80755ce3a1e2925b1aa33c2614
Varatlan <148843870+Varatlan@users.noreply.github.com>
2023-10-29 11:46:01 +0700

    Add files via upload

---
+++ PBO6/Rusak/src/rusak/Tugas.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rusak;
+
+/**
+ *
+ * @author RIGISEFA
+ */
+public class Tugas {
+    private String nim;
+    private String nama;
+    private String domisili;
+    public String getNim() {
+        return nim;
+    }
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getDomisili() {
+        return domisili;
+    }
+
+    public void setDomisili(String domisili) {
+        this.domisili = domisili;
+    }
+}
